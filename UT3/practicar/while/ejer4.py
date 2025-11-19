2f40bdc1a431a6672d3cc3521bfd316a9c50bb6b
Mrclobbers <gfernandezj08@gmail.com>
2025-11-19 16:43:37 +0100

    Actualización de los ejercicios

--- UT3/practicar/while/ejer4.py
+++ UT3/practicar/while/ejer4.py
@@ -1,14 +1,11 @@
-texto = str(input('Dime un caracter: '))
+car = ""
 
-while len(texto) < 1:
-    while texto == '1' or texto == '2' or texto == '3' or texto == '4' or texto == '5' or texto == '6' or texto == '7' or texto == '8' or texto == '9' or texto == '0':
-        print('No has escrito un caracter, vuelve a escribirlo.')
-        texto = str(input('Dime un caracter: '))
+while len(car) != 1: # Asegurarse de que el carácter es solo uno
+    car = input("Introduce un carácter: ")
+
+while car != " ":
+    if car.lower() in 'aeiou':
+        print("VOCAL")
     else:
-        while texto != ' ':
-            if texto == 'a' or texto == 'e' or texto == 'i' or texto == 'o' or texto == 'u':
-                print('VOCAL')
-                texto = str(input('Dime otro caracter: '))
-            else:
-                print('NO VOCAL')
-                texto = str(input('Dime un caracter: '))
+        print("NO VOCAL")
+    break
---
+++ UT3/practicar/while/ejer6.py
@@ -0,0 +1,6 @@
+num_tabla = int(input("¿De qué número quieres mostrar la tabla de multiplicar?: "))
+
+num=1
+while num<=10:
+    print(f" {num_tabla} * {num} = {num * num_tabla}")
+    num+=1
